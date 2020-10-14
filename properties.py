619881b2d7484f24c820e4d7ca90d6a2700700ef
Lemikhov Alex <lemikhovalex@gmail.com>
2020-10-14 19:45:08 +0300

    more properties

--- properties.py
+++ properties.py
@@ -1,18 +1,33 @@
 class Constants:
     def __init__(self):
-        self.__mu_water = 5.531e-7  # m^2 / s
-        self.__mu_oil = 2.86e-6  # m^2 / s
-        self.__B_w = 1  # relative
-        self.__B_o = 1  # relative
-        self.__c_o = 45.8e-11   # Pa^-1
-        self.__c_w = 45.8e-11  # Pa^-1
-        self.__c_r = 1e-9  # Pa^-1
-        self.__k_r_o = 1  # relative
-        self.__k_r_w = 1  # relative
-        self.__k_avg = 1 * 1.987e-13  # 1 darcy as m^2
-        self.dt = 10  # s
-        self.dx = 100  # m
-        self.dy = 100  # m
+        self.__mu_water = 5.531e-4  # m^2 / s
+        self.__mu_oil = 2.9e-6  # m^2 / s
+        self.__B_w = 1.  # relative
+        self.__B_o = 1.  # relative
+        self.__c_o = 15.8e-10   # Pa^-1
+        self.__c_w = 15.8e-10  # Pa^-1
+        self.__c_r = 1e-0  # Pa^-1
+        self.__k_r_o = 1.  # relative
+        self.__k_r_w = 1.  # relative
+        self.__k_avg = 1 * 1.987e-13  # 1 darcy to m^2
+        self.__dt = 0.5  # s
+        self.__dx = 5.  # m
+        self.__dy = 5.  # m
+        self.__d_avg = 50.  # m
+        self.__p_0 = 4e4 * 6894  # psi to Pa
+        self.__dx = 1.
+
+    def dx(self):
+        return float(self.__dx)
+
+    def dy(self):
+        return self.__dy
+
+    def depth_avg(self):
+        return self.__d_avg
+
+    def dt(self):
+        return self.__dt
 
     def mu_water(self):
         return self.__mu_water
@@ -35,6 +50,9 @@ class Constants:
     def k_r_w(self):
         return self.__k_r_w
 
+    def k_avg(self):
+        return self.__k_avg
+
     def c_r(self):
         return self.__c_r
 
@@ -43,3 +61,6 @@ class Constants:
 
     def c_o(self):
         return self.__c_o
+
+    def p_0(self):
+        return self.__p_0
