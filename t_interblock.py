b82a701bd5cc9bdfa07a4225cfd81c3990796995
Lemikhov Alex <lemikhovalex@gmail.com>
2020-10-14 19:43:18 +0300

    bugs with init

--- t_interblock.py
+++ t_interblock.py
@@ -1,6 +1,7 @@
 import numpy as np
 import utils as u
 from d_matrix import DMatrix
+from k_matrix import KMatrix
 from math import ceil, floor
 
 
@@ -11,18 +12,15 @@ class TInterBlockMatrix:
     """
 
     def __init__(self,
-                 k_values: np.ndarray,
+                 k_matrix: KMatrix,
                  dx_matrix: np.array,
                  dy_matrix: np.array,
                  d_matrix: DMatrix):
-        self.__k_matrix = u.KMatrix(k_values, dy_matrix, dx_matrix)
+        self.__k_matrix = k_matrix
         self.__d_matrix = d_matrix
         self.__dx_matrix = dx_matrix
         self.__dy_matrix = dy_matrix
-        assert k_values.shape[0] == dx_matrix.shape[0], 'input shape issue, k_values and dx'
-        assert k_values.shape[1] == dy_matrix.shape[0], 'input shape issue k_values and dy'
-        assert k_values.shape == d_matrix.shape, 'input shape issue k values and depth'
-        self.shape = k_values.shape
+        self.shape = k_matrix.shape
 
     def __getitem__(self, item):
         """
@@ -39,37 +37,37 @@ class TInterBlockMatrix:
             if (i == -0.5) & (j <= ny - 1) & (0 <= j) & u.check_int(j):
                 i = 0
                 out = self.__d_matrix[i, j] * self.__dy_matrix[floor(i)] * self.__k_matrix[i, j]
-                out /= (self.__dx_matrix(floor(j)) + self.__dx_matrix(ceil(j))) / 2
+                out /= (self.__dx_matrix[floor(j)] + self.__dx_matrix[ceil(j)]) / 2
                 return 2 * out
             # one of bound
             if (i == nx - 0.5) & (j <= ny - 1) & (0 <= j) & u.check_int(j):
                 i = nx - 1
                 out = self.__d_matrix[i, j] * self.__dy_matrix[floor(i)] * self.__k_matrix[i, j]
-                out /= (self.__dx_matrix(floor(j)) + self.__dx_matrix(ceil(j))) / 2
+                out /= (self.__dx_matrix[floor(j)] + self.__dx_matrix[ceil(j)]) / 2
                 return 2 * out
 
             # other 2 line bounds
             if (j == -0.5) & (i <= nx - 1) & (0 <= i) & u.check_int(i):
                 j = 0
                 out = self.__d_matrix[i, j] * self.__dx_matrix[floor(i)] * self.__k_matrix[i, j]
-                out /= (self.__dy_matrix(floor(j)) + self.__dy_matrix(ceil(j))) / 2
+                out /= (self.__dy_matrix[floor(j)] + self.__dy_matrix[ceil(j)]) / 2
                 return 2 * out
             # bound
-            if (j == ny - 0.5) & (j <= ny - 1) & (0 <= j) & u.check_int(j):
+            if (j == ny - 0.5) & (i <= nx - 1) & (0 <= i) & u.check_int(i):
                 j = ny - 1
                 out = self.__d_matrix[i, j] * self.__dx_matrix[floor(i)] * self.__k_matrix[i, j]
-                out /= (self.__dy_matrix(floor(j)) + self.__dy_matrix(ceil(j))) / 2
+                out /= (self.__dy_matrix[floor(j)] + self.__dy_matrix[ceil(j)]) / 2
                 return 2 * out
 
             # major cases
             if u.check_int(i) & u.check_half(j):
                 out = self.__d_matrix[i, j] * self.__dx_matrix[floor(i)] * self.__k_matrix[i, j]
-                out /= (self.__dy_matrix(floor(j)) + self.__dy_matrix(ceil(j))) / 2
+                out /= (self.__dy_matrix[floor(j)] + self.__dy_matrix[ceil(j)]) / 2
                 return out
 
             elif u.check_half(i) & u.check_int(j):
                 out = self.__d_matrix[i, j] * self.__dy_matrix[floor(i)] * self.__k_matrix[i, j]
-                out /= (self.__dx_matrix(floor(j)) + self.__dx_matrix(ceil(j))) / 2
+                out /= (self.__dx_matrix[floor(j)] + self.__dx_matrix[ceil(j)]) / 2
                 return out
             else:
                 assert False, "wrong index, not int + int and a half-like int"
